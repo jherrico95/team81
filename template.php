9d665dda92d69a00edc5d483002e701674207833
JD Legaspi <JD Legaspi>
2016-09-03 16:49:44 +1000

    added structure

--- template.php
+++ template.php
@@ -1,13 +1,41 @@
 <?php
-	include('hml_head.inc');
+	include 'hml_head.inc';
 ?>
-			<div id="content_area">
-				<?php echo $content; ?>
-			</div>
-			
-			<div id="sidebar_index">
-				<?php echo $sidebar; ?>
-			</div>
-	<?php
-		include('footer.inc');
-	?>
+
+<div class="top-header">
+	
+	<div class="col-md-7">
+	</div>
+	
+	<div class="col-md-5">
+	</div>
+
+</div>
+
+<div class="home-page-content">
+	
+	<!--banner div should have fixed width-->
+	<div id="home-banner">
+	</div>
+
+	<div class="home-elements">
+		
+		<div class="col-md-3">
+		</div>
+
+		<div class="col-md-3">
+		</div>
+
+		<div class="col-md-3">
+		</div>
+
+		<div class="col-md-3">
+		</div>
+
+	</div>
+
+</div>
+
+<?php
+	include('footer.inc');
+?>
