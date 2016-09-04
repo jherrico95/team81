6baf31def324cfeefc1addd4d85a0989b2474fe3
somekindoftiz <tylerbrockmurphy@gmail.com>
2016-09-04 19:58:22 +1000

    query test

--- login.php
+++ login.php
@@ -17,15 +17,15 @@
 		include('checkpasswd.inc');
 		if (checkPassword($_POST['userName'], $_POST['passwd'], $pdo)){
 			
-			$typeQuery = $pdo->prepare('SELECT userType FROM login WHERE userName = :userName');
+			//$typeQuery = $pdo->prepare('SELECT userType FROM login WHERE userName = :userName');
 			//$typeQuery->bindParam(':userName', $_POST['userName'], PDO::PARAM_STR);
-			$typeQuery->bindValue(':userName', $userName);
-			$typeQuery->execute();
+			//$uery->bindValue(':userName', $userName);
+			//$typeQuery->execute();
 
 			echo 'Login works';
 			session_start(); 
 			$_SESSION['isMembers'] = true;
-			$_SESSION['userType'] = $typeQuery;
+			$_SESSION['userType'] = 'admin';
 			$_SESSION['userName'] = $_POST['userName'];
 			header('Location: /team81/index.php');
 			exit();
