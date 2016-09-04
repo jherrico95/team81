9383101e549d3cfaeb8badc25217f95d76886357
somekindoftiz <tylerbrockmurphy@gmail.com>
2016-09-05 08:42:39 +1000

    changed teacher_approval to admin.php, added admin_template

---
+++ admin.php
@@ -0,0 +1,14 @@
+<?php
+$title = "Admin - Teacher Approval";
+$content = "<img src='images/parkbark.png' al=''/>";
+$sidebar = "erfjkgrelkjbw;kjfbwe";
+
+session_start();
+if($_SESSION['userType'] != 'admin')
+{
+	header('Location: /team81/index.php');
+	
+}else if($_SESSION['userType'] == 'admin') {
+	include 'admin_template.php';
+}
+?>
---
+++ template_admin.php
@@ -0,0 +1,43 @@
+<?php
+	include 'hml_head.inc';
+?>
+
+<div class="top-header">
+	
+	<div class="col-md-7">
+	</div>
+	
+	<div class="col-md-5">
+	</div>
+
+</div>
+
+<div class="home-page-content">
+	
+	<!--banner div should have fixed width-->
+	<div id="home-banner">
+	</div>
+
+	<div class="home-elements">
+		
+		<div class="col-md-3">
+		</div>
+
+		<div class="col-md-3">
+		</div>
+
+		<div class="col-md-3">
+		</div>
+
+		<div class="col-md-3">
+		</div>
+
+	</div>
+	nhbnjvfpkornjvcpkorvgjf
+
+
+</div>
+
+<?php
+	include('footer.inc');
+?>
