e5585cb6fcf39a28d5184c92b37e1ae3a93e6b86
somekindoftiz <tylerbrockmurphy@gmail.com>
2016-09-05 09:45:10 +1000

    added teacherchange function

--- template_admin.php
+++ template_admin.php
@@ -60,7 +60,7 @@
 		echo $e->getMessage();  
 		}
 		echo '<form action="admin.php"  method="POST">';
-			echo'<select>';
+			echo'<select name ="teacher">';
 				echo'<option value="ALL">SELECT STUDENT</option>';
 				foreach ($students as $student){
 					echo'<option value=',$student['userName'],'>',$student['userName'],'</option>';
