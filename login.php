2ce4a421758983e2dbfe3d83f1adebe50a69c70a
somekindoftiz <tylerbrockmurphy@gmail.com>
2016-09-04 19:48:48 +1000

    welcome test

--- login.php
+++ login.php
@@ -24,7 +24,7 @@
 			echo 'Login works';
 			session_start(); 
 			$_SESSION['isMembers'] = true;
-			$_SESSION['userType'] = 'teacher';
+			$_SESSION['userType'] = 'admin';
 			$_SESSION['userName'] = $_POST['userName'];
 			header('Location: /team81/index.php');
 			exit();
