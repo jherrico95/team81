f05acb8e94d98ecaffb85f319fb561823e5864c1
somekindoftiz <tylerbrockmurphy@gmail.com>
2016-09-05 08:36:07 +1000

    added teacher approval page - added not isset - added sessionstart

--- teacher_approval.php
+++ teacher_approval.php
@@ -3,7 +3,7 @@ $title = "Admin - Teacher Approval";
 $content = "<img src='images/parkbark.png' al=''/>";
 $sidebar = "erfjkgrelkjbw;kjfbwe";
 
-
+session_start();
 if($_SESSION['userType'] != 'admin')
 {
 	header('Location: /team81/index.php');
