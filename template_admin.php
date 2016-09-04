47478a8620f63886d58df126015891a2106f3dce
somekindoftiz <tylerbrockmurphy@gmail.com>
2016-09-05 08:59:47 +1000

    added a form to template admin

--- template_admin.php
+++ template_admin.php
@@ -33,8 +33,42 @@
 		</div>
 
 	</div>
-	nhbnjvfpkornjvcpkorvgjf
+	Change a student into a teacher:
+	<?php
+		include('createDB.inc');
 
+		// $stmt = $pdo->prepare('INSERT INTO login (userType, userName, salt, password) 
+		// 						VALUES (:student, :userName, :salt, SHA2(CONCAT(:passwd, :salt), 0))');
+		// $stmt->bindValue(':userName', $_POST['userName']);
+		// //$stmt->bindValue(':email', $_POST['email']);
+		// $stmt->bindValue(':student', 'student');
+		// $stmt->bindValue(':passwd', $_POST['passwd']);
+		// $stmt->bindValue(':salt', $salt);
+		
+		// $stmt->execute();
+
+
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+		try  
+		{  
+		$students = $pdo->query('SELECT * FROM login WHERE userType == "student"'); 
+		//$result2 = $pdo->query('SELECT id, dogParkName, street, parkName, latitude, longitude, dogParkArea, Picture, imageAlt FROM parks'); 
+		//$suburb1 = $pdo->query('SELECT distinct suburb  FROM parks');
+		}
+		catch (PDOException $e)  
+		{   
+		echo $e->getMessage();  
+		}
+		echo '<form>';
+		echo'<select>';
+			echo'<option value="ALL">ALL</option>';
+			foreach ($students as $student){
+				echo'<option value=',$students['userType'],'>',$students['userType'],'</option>';
+			}
+		echo'</select>';
+		echo'</form>';
+
+	?>
 
 </div>
 
