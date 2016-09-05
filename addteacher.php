47fa36666e68ab184b1bd50b62445496376da9e9
somekindoftiz <tylerbrockmurphy@gmail.com>
2016-09-05 10:51:41 +1000

    addteacher.php teacher test

--- addteacher.php
+++ addteacher.php
@@ -1,8 +1,8 @@
 <?php
 	$teacher = $_POST['teacher'];
 	include('createDB.inc');
-	echo'<li><a href="logout.php">'$teacher'</a><li>';
-	echo $teacher;
+	//echo'<li><a href="logout.php">'$teacher'</a><li>';
+	echo '<p>'$teacher'</p>';
 	// function addTeacher($userName){
 		
 	//  	$teacher = $pdo->query('UPDATE login SET userType="teacher" WHERE userName = :userName '); 
