1383438ccf7dce7342269333a28b6976b9f61646
somekindoftiz <tylerbrockmurphy@gmail.com>
2016-09-05 15:36:46 +1000

    added contact us info

---
+++ contact_template.php
@@ -0,0 +1,57 @@
+<?php
+	include 'hml_head.inc';
+
+?>
+
+<div class="top-header">
+	
+	<div class="col-md-7">
+	</div>
+	
+	<div class="col-md-5">
+	</div>
+
+</div>
+
+<div class="home-page-content">
+	
+	<!--banner div should have fixed width-->
+	<div id="home-banner">
+	</div>
+
+	<div class="home-elements">
+		
+		<div class="col-md-3">
+		</div>
+
+		<div class="col-md-3">
+		</div>
+
+		<div class="col-md-3">
+		</div>
+
+		<div class="col-md-3">
+		</div>
+
+		 <div id="map"></div>
+    <script>
+      function initMap() {
+        var mapDiv = document.getElementById('map');
+        var map = new google.maps.Map(mapDiv, {
+            center: {lat: 44.540, lng: -78.546},
+            zoom: 8
+        });
+      }
+    </script>
+    <script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDYOertwWLoBJGxLJhp8Z3Bu-mlOIFdnA4&callback=initMap">
+    </script>
+
+
+	</div>
+
+</div>
+
+<?php
+	include('footer.inc');
+?>
