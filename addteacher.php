c84aa0feb0f43bfa255cc0bb75f7abe57b7fa26f
somekindoftiz <tylerbrockmurphy@gmail.com>
2016-09-05 13:13:00 +1000

    addteacher.php teacher test

--- addteacher.php
+++ addteacher.php
@@ -1,27 +1,18 @@
 <?php
 	$teacher = $_POST['teacher'];
-	include('createDB.inc');
+	//include('createDB.inc');
 	//echo'<li><a href="logout.php">'$teacher'</a><li>';
-	echo '<p>'$teacher'</p>';
+	// echo '<p>'$teacher'</p>';
 
-try  
-	{  
-	
-	
-	
-	 function addTeacher($userName){
+	//  function addTeacher($userName){
 		
-	  	$teacher = $pdo->query('UPDATE login SET userType="teacher" WHERE userName = :userName '); 
-	  	$teacher->bindValue(':userName', $userName);
-	  	$teacher->execute();
+	//   	$teacher = $pdo->query('UPDATE login SET userType="teacher" WHERE userName = :userName '); 
+	//   	$teacher->bindValue(':userName', $userName);
+	//   	$teacher->execute();
 		
-	  	header('Location: /team81/admin.php');
-	}	
-	 addTeacher($teacher);
-	}
-catch (PDOException $e)  
-	{   
-	echo $e->getMessage();  
-	}
+	//   	header('Location: /team81/admin.php');
+	// }	
+	//  addTeacher($teacher);
+
 
 ?>
