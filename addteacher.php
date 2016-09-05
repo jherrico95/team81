0f7d2b0faac682250c8f67dad68e79da6c92ac80
somekindoftiz <tylerbrockmurphy@gmail.com>
2016-09-05 10:24:59 +1000

    addteacher.php

--- addteacher.php
+++ addteacher.php
@@ -1,21 +1,12 @@
 <?php
-	//include('createDB.inc');
+	include('createDB.inc');
 	addTeacher($_POST['teacher']);
-	 function addTeacher($userName){
+	function addTeacher($userName){
 		
 	 	$teacher = $pdo->query('UPDATE login SET userType="teacher" WHERE userName = :userName '); 
 	 	$teacher->bindValue(':userName', $userName);
 	 	$teacher->execute();
-		// $query = $pdo->prepare('SELECT * FROM login WHERE userName = :userName 
-		// 						and password = SHA2(CONCAT(:passwd, salt), 0)');
-		// $query->bindValue(':passwd', $passwd);
-		// $query->bindValue(':userName', $userName);
 		
-		// $query->execute();
-		// //$_SESSION['userType'] = $query['userType'];
-		// if($query->rowCount() > 0){
-		// return True;
-		// }
 	 	header('Location: /team81/admin.php');
 	}	
 	
