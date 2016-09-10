3455cf11425fb40ec859d6549be20b30ffe544ab
somekindoftiz <tylerbrockmurphy@gmail.com>
2016-09-11 08:59:28 +1000

    commented out table in teachers

--- templates/teachers_template.php
+++ templates/teachers_template.php
@@ -52,34 +52,34 @@
 		}
 		
 
-		// foreach ($teachers as $teacher){
-		// 	echo'<table width="100%">';
-		// 		echo'<table>';
-		// 			echo '<tr>';
-		// 				//echo '<td><img width="100px" src="images/',$park2['Picture'],'"alt="',$park2['imageAlt'],'"></td>';
-		// 			echo '</tr>';
-		// 		echo '</table>';
+		foreach ($teachers as $teacher){
+			echo'<table width="100%">';
+				echo'<table>';
+					echo '<tr>';
+						//echo '<td><img width="100px" src="images/',$park2['Picture'],'"alt="',$park2['imageAlt'],'"></td>';
+					echo '</tr>';
+				echo '</table>';
 				
 				
-		// 		echo'<table>';
-		// 			echo'<tr>';
-		// 				echo '<td><a href="lesson.php?teacher=',$teacher['accountNo'],'">Book a lesson</a></td>';
-		// 			echo'</tr>';
-		// 			//echo $park['dogParkName'];
-		// 			echo'<tr>';
-		// 				echo '<td>';
-		// 					echo $teacher['firstName'];
-		// 				echo '</td>';
-		// 				echo '<td>';
-		// 					echo $teacher['lastName'];
-		// 				echo '</td>';
-		// 				echo '<td>';
-		// 					echo '<p>',$teacher['email'],'<nbsp>m2</p>';
-		// 				echo '</td>';
+				echo'<table>';
+					echo'<tr>';
+						echo '<td><a href="lesson.php?teacher=',$teacher['accountNo'],'">Book a lesson</a></td>';
+					echo'</tr>';
+					//echo $park['dogParkName'];
+					echo'<tr>';
+						echo '<td>';
+							echo $teacher['firstName'];
+						echo '</td>';
+						echo '<td>';
+							echo $teacher['lastName'];
+						echo '</td>';
+						echo '<td>';
+							echo '<p>',$teacher['email'],'<nbsp>m2</p>';
+						echo '</td>';
 					 
-		// 			echo'</tr>';
-		// 		echo '</table>';
-		// 	echo '</table>';
+					echo'</tr>';
+				echo '</table>';
+			echo '</table>';
 	?>
 
 </div>
