79a06e11d84af5937151b0ea92d97a5a8fe16197
somekindoftiz <tylerbrockmurphy@gmail.com>
2016-09-11 09:34:51 +1000

    added 404 page

---
+++ 404page.php
@@ -0,0 +1,8 @@
+<?php
+$title = "404 - Pinelands Music School";
+$content = "<img src='images/parkbark.png' al=''/>";
+$sidebar = "erfjkgrelkjbw;kjfbwe";
+
+session_start();
+include 'templates/template_404.php';
+?>
---
+++ templates/template_404.php
@@ -0,0 +1,43 @@
+<?php
+	include 'html_head.inc';
+?>
+
+<div class="top-header">
+	
+	<div class="col-md-7">
+	</div>
+	
+	<div class="col-md-5">
+	</div>
+
+</div>
+
+<div class="home-page-content">
+	
+	<!--banner div should have fixed width-->
+	<div id="home-banner">
+	</div>
+
+	<div class="home-elements">
+		
+		<div class="col-md-3">
+		</div>
+
+		<div class="col-md-3">
+		</div>
+
+		<div class="col-md-3">
+		</div>
+
+		<div class="col-md-3">
+		</div>
+
+	</div>
+
+	<h1>Im sorry this page does not Exist - 404 Error</h1>
+
+</div>
+
+<?php
+	include('footer.inc');
+?>
