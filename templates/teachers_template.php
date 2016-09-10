3fd137689ca26d4389643e84580dcd3e613b536d
somekindoftiz <tylerbrockmurphy@gmail.com>
2016-09-11 08:55:19 +1000

    commented out table in teachers

--- templates/teachers_template.php
+++ templates/teachers_template.php
@@ -39,47 +39,47 @@
 		
 
 
-		$pdo4->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
-		try  
-		{  
-		$teachers = $pdo4->query('SELECT * FROM "teachers"'); 
-		//$result2 = $pdo->query('SELECT id, dogParkName, street, parkName, latitude, longitude, dogParkArea, Picture, imageAlt FROM parks'); 
-		//$suburb1 = $pdo->query('SELECT distinct suburb  FROM parks');
-		}
-		catch (PDOException $e)  
-		{   
-		echo $e->getMessage();  
-		}
+		// $pdo4->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+		// try  
+		// {  
+		// $teachers = $pdo4->query('SELECT * FROM "teachers"'); 
+		// //$result2 = $pdo->query('SELECT id, dogParkName, street, parkName, latitude, longitude, dogParkArea, Picture, imageAlt FROM parks'); 
+		// //$suburb1 = $pdo->query('SELECT distinct suburb  FROM parks');
+		// }
+		// catch (PDOException $e)  
+		// {   
+		// echo $e->getMessage();  
+		// }
 		
 
-		foreach ($teachers as $teacher){
-			echo'<table width="100%">';
-				echo'<table>';
-					echo '<tr>';
-						//echo '<td><img width="100px" src="images/',$park2['Picture'],'"alt="',$park2['imageAlt'],'"></td>';
-					echo '</tr>';
-				echo '</table>';
+		// foreach ($teachers as $teacher){
+		// 	echo'<table width="100%">';
+		// 		echo'<table>';
+		// 			echo '<tr>';
+		// 				//echo '<td><img width="100px" src="images/',$park2['Picture'],'"alt="',$park2['imageAlt'],'"></td>';
+		// 			echo '</tr>';
+		// 		echo '</table>';
 				
 				
-				echo'<table>';
-					echo'<tr>';
-						echo '<td><a href="lesson.php?teacher=',$teacher['accountNo'],'">Book a lesson</a></td>';
-					echo'</tr>';
-					//echo $park['dogParkName'];
-					echo'<tr>';
-						echo '<td>';
-							echo $teacher['firstName'];
-						echo '</td>';
-						echo '<td>';
-							echo $teacher['lastName'];
-						echo '</td>';
-						echo '<td>';
-							echo '<p>',$teacher['email'],'<nbsp>m2</p>';
-						echo '</td>';
+		// 		echo'<table>';
+		// 			echo'<tr>';
+		// 				echo '<td><a href="lesson.php?teacher=',$teacher['accountNo'],'">Book a lesson</a></td>';
+		// 			echo'</tr>';
+		// 			//echo $park['dogParkName'];
+		// 			echo'<tr>';
+		// 				echo '<td>';
+		// 					echo $teacher['firstName'];
+		// 				echo '</td>';
+		// 				echo '<td>';
+		// 					echo $teacher['lastName'];
+		// 				echo '</td>';
+		// 				echo '<td>';
+		// 					echo '<p>',$teacher['email'],'<nbsp>m2</p>';
+		// 				echo '</td>';
 					 
-					echo'</tr>';
-				echo '</table>';
-			echo '</table>';
+		// 			echo'</tr>';
+		// 		echo '</table>';
+		// 	echo '</table>';
 	?>
 
 </div>
