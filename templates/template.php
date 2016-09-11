e0c06b637a4c19e6d3f22d2fe7fcb591dee1e654
JDLEGASPI-PC\JD Legaspi <jdalegaspi@gmail.com>
2016-09-11 14:09:34 +1000

    Changed front end look, still in progress

--- templates/template.php
+++ templates/template.php
@@ -2,39 +2,261 @@
 	include 'html_head.inc';
 ?>
 
-<div class="top-header">
+<!-- SLIDER -->
+	<section id="slider">
+		<div id="home-carousel" class="carousel slide" data-ride="carousel">			
+			<div class="carousel-inner">
+				<div class="item active" style="background-image: url(images/slider/music_school.jpg)">
+					<div class="carousel-caption container">
+						<div class="row">
+							<div class="col-sm-7">
+								<h1>Learn, Improve, Inspire </h1>
+								<h2 style="color: #a2c4c9">be the music</h2>
+								<p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor. Aenean sollicitudin, lorem quis bibendum auctor.</p>
+							</div>
+						</div>
+					</div>					
+				</div>
+				<!--<a class="home-carousel-left" href="#home-carousel" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+				<a class="home-carousel-right" href="#home-carousel" data-slide="next"><i class="fa fa-angle-right"></i></a>-->
+			</div>		
+		</div> <!--/#home-carousel--> 
+    </section>
+	<!-- /SLIDER -->
+
 	
-	<div class="col-md-7">
-	</div>
+	<!-- SERVICES -->
+	<section id="services">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="section-title">
+						<h1>Services</h1>
+						<span class="st-border"></span>
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-6 st-service">
+					<h2><i class="fa fa-desktop"></i> Web Design</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta libero autem, magni veritatis, optio dolor.</p>
+				</div>
+
+				<div class="col-md-4 col-sm-6 st-service">
+					<h2><i class="fa fa-cogs"></i> Web Developement</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta libero autem, magni veritatis, optio dolor.</p>
+				</div>
+
+				<div class="col-md-4 col-sm-6 st-service">
+					<h2><i class="fa fa-code"></i> Custom Development</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta libero autem, magni veritatis, optio dolor.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- /SERVICES -->
+
+
+	<!-- OUR TEAM -->
+	<section id="our-team">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="section-title">
+						<h1>Our Team</h1>
+						<span class="st-border"></span>
+					</div>
+				</div>
+
+				<div class="col-md-3 col-sm-6">
+					<div class="team-member">
+						<div class="member-image">
+							<img class="img-responsive" src="images/members/team1.jpg" alt="">
+							<div class="member-social">
+								<a href=""><i class="fa fa-facebook"></i></a>
+								<a href=""><i class="fa fa-twitter"></i></a>
+								<a href=""><i class="fa fa-google-plus"></i></a>
+								<a href=""><i class="fa fa-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Khalil Uddin</h4>
+							<span>Designer</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-3 col-sm-6">
+					<div class="team-member">
+						<div class="member-image">
+							<img class="img-responsive" src="images/members/team2.jpg" alt="">
+							<div class="member-social">
+								<a href=""><i class="fa fa-facebook"></i></a>
+								<a href=""><i class="fa fa-twitter"></i></a>
+								<a href=""><i class="fa fa-google-plus"></i></a>
+								<a href=""><i class="fa fa-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Rubel Miah</h4>
+							<span>Lead Developer</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-3 col-sm-6">
+					<div class="team-member">
+						<div class="member-image">
+							<img class="img-responsive" src="images/members/team1.jpg" alt="">
+							<div class="member-social">
+								<a href=""><i class="fa fa-facebook"></i></a>
+								<a href=""><i class="fa fa-twitter"></i></a>
+								<a href=""><i class="fa fa-google-plus"></i></a>
+								<a href=""><i class="fa fa-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Shamim Shohag</h4>
+							<span>Developer</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-3 col-sm-6">
+					<div class="team-member">
+						<div class="member-image">
+							<img class="img-responsive" src="images/members/team2.jpg" alt="">
+							<div class="member-social">
+								<a href=""><i class="fa fa-facebook"></i></a>
+								<a href=""><i class="fa fa-twitter"></i></a>
+								<a href=""><i class="fa fa-google-plus"></i></a>
+								<a href=""><i class="fa fa-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Zubayer Rahman</h4>
+							<span>Marketer</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- /OUR TEAM -->
+
+
+	<!-- ABOUT US -->
+	<section id="about-us">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="about-us text-center">
+						<h4>Morem ipsum dolor sit amet.</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum aliquid obcaecati deleniti minus quisquam quis magnam reprehenderit quaerat molestias rerum, excepturi autem fugiat corporis quidem a ipsum quos beatae! Ut!</p>
+						<a href="" class="btn btn-send">View More</a>
+					</div>
+				</div>
+				<div class="col-sm-6 our-office">
+					<div id="office-carousel" class="carousel slide" data-ride="carousel">			
+						<div class="carousel-inner">
+							<div class="item active">
+								<img src="images/office/04.jpg" alt="">
+							</div>
+							<div class="item">
+								<img src="images/office/02.jpg" alt="">			
+							</div>
+							<div class="item">
+								<img src="images/office/03.jpg" alt="">			
+							</div>
+							
+							<a class="office-carousel-left" href="#office-carousel" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+							<a class="office-carousel-right" href="#office-carousel" data-slide="next"><i class="fa fa-angle-right"></i></a>
+						</div>		
+					</div> <!--/#office-carousel--> 
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- /ABOUT US -->
 	
-	<div class="col-md-5">
-	</div>
+	<!-- TESTIMONIAL -->
+	<section id="testimonial">
+		<div class="container">
+			<div class="row">
+				<div class="overlay"></div>
+				<div class="col-md-8 col-md-offset-2 col-sm-12">
+					<div class="st-testimonials">
 
-</div>
+						<div class="item active text-center">
+							<p>"As a student of Pinelands Music School, I feel like I've learn how to play music well and stuff and, like, the teachers and lit af"</p>
+							<div class="st-border"></div>
+							<div class="client-info">
+								<h5>xXx420BlazexXx</h5>
+								<span>Student - Grade 7</span>
+							</div>
+						</div>
 
-<div class="home-page-content">
+						<div class="item text-center">
+							<p>"I'm a teacher at Pinelands Music School teaching the traditional Khura'inese string instrument Dahmalan. I find the new webapp very easy to use, and allows me to more easily set up lessons with students."</p>
+							<div class="st-border"></div>
+							<div class="client-info">
+								<h5>Pees'lubn Andistan'dhin</h5>
+								<span>Head Monk of Tehm'pul Temple</span>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- /TESTIMONIAL -->
 	
-	<!--banner div should have fixed width-->
-	<div id="home-banner">
-	</div>
-
-	<div class="home-elements">
-		
-		<div class="col-md-3">
+	<!-- CONTACT -->
+	<section id="contact">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="section-title">
+						<h1>Contact us</h1>
+						<span class="st-border"></span>
+					</div>
+				</div>
+				<div class="col-sm-4 contact-info">
+					<p class="contact-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae voluptatum dolorum, possimus tenetur pariatur officia, quo commodi autem doloribus vero rerum aspernatur quidem temporibus.</p>
+					<p class="st-address"><i class="fa fa-map-marker"></i> <strong>E71 8th Ave, New York NY 21001, US</strong></p>
+					<p class="st-phone"><i class="fa fa-mobile"></i> <strong>+00 123-456-789</strong></p>
+					<p class="st-email"><i class="fa fa-envelope-o"></i> <strong>email@yourdomain.com</strong></p>
+					<p class="st-website"><i class="fa fa-globe"></i> <strong>www.yourdomain.com</strong></p>
+				
+				</div>
+				<div class="col-sm-7 col-sm-offset-1">
+					<form action="templates/send-contact.php" class="contact-form" name="contact-form" method="post">
+						<div class="row">
+							<div class="col-sm-6">
+								<input type="text" name="name" required="required" placeholder="Name*">
+							</div>
+							<div class="col-sm-6">
+								<input type="email" name="email" required="required" placeholder="Email*">
+							</div>
+							<div class="col-sm-6">
+								<input type="text" name="subject" placeholder="Subject">
+							</div>
+							<div class="col-sm-6">
+								<input type="text" name="website" placeholder="Website">
+							</div>
+							<div class="col-sm-12">
+								<textarea name="message" required="required" cols="30" rows="7" placeholder="Message*"></textarea>
+							</div>
+							<div class="col-sm-12">
+								<input type="submit" name="submit" value="Send Message" class="btn btn-send">
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
 		</div>
-
-		<div class="col-md-3">
-		</div>
-
-		<div class="col-md-3">
-		</div>
-
-		<div class="col-md-3">
-		</div>
-
-	</div>
-
-</div>
+	</section>
+	<!-- /CONTACT -->
 
 <?php
 	include('footer.inc');
