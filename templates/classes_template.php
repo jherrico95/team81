328811f9cffe471725edb26659c7014e88a28178
JDLEGASPI-PC\JD Legaspi <jdalegaspi@gmail.com>
2016-10-26 01:08:12 +1000

    Front End Updates

--- templates/classes_template.php
+++ templates/classes_template.php
@@ -2,79 +2,49 @@
 	include 'html_head.inc';
 ?>
 
-<div class="top-header">
-	
-	<div class="col-md-7">
+<div class="form-container">
+	<h2>Classes</h2>
+	<div class="classes-form">
+		<?php
+			include('createDB.inc');
+			$pdo8->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+			try  
+			{  
+			$courses = $pdo8->query('SELECT * FROM courses'); 
+			//$result2 = $pdo->query('SELECT id, dogParkName, street, parkName, latitude, longitude, dogParkArea, Picture, imageAlt FROM parks'); 
+			//$suburb1 = $pdo->query('SELECT distinct suburb  FROM parks');
+			}
+			catch (PDOException $e)  
+			{   
+			echo $e->getMessage();  
+			}
+			echo '<table>';
+				//echo'<select name="teacher">';
+					//echo'<option value="" selected="selected">SELECT STUDENT</option>';
+				echo'<tr>
+					<th>Course Name</th>
+					<th>Cost</th>
+					<th>Number of Lessons</th>
+					<th>Instrument</th>
+					<th>Available</th>
+					<th></th>
+				</tr>';
+					foreach ($courses as $course){
+						echo'<tr>
+								<td>',$course['courseName'],'</td>
+								<td>',$course['courseCost'],'</td>
+								<td>',$course['numberOfLessons'],'</td>
+								<td>',$course['instrumentType'],'</td>
+								<td>',$course['available'],'</td>
+								<td><a href="enroll.php?course=',$course['courseID'],'">Enrol in Course</a></td>
+							</tr>';
+					}
+				//echo'</select>';
+				//echo'<input type="submit" name="SUBMIT">';
+			echo'</table>';
+
+		?>
 	</div>
-	
-	<div class="col-md-5">
-	</div>
-
-</div>
-
-<div class="home-page-content">
-	
-	<!--banner div should have fixed width-->
-	<div id="home-banner">
-	</div>
-
-	<div class="home-elements">
-		
-		<div class="col-md-3">
-		</div>
-
-		<div class="col-md-3">
-		</div>
-
-		<div class="col-md-3">
-		</div>
-
-		<div class="col-md-3">
-		</div>
-
-	</div>
-	<br><br><br><br><br><br>
-	<?php
-		include('createDB.inc');
-		
-		echo '<h2>Courses</h2>';
-		$pdo8->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
-		try  
-		{  
-		$courses = $pdo8->query('SELECT * FROM courses'); 
-		//$result2 = $pdo->query('SELECT id, dogParkName, street, parkName, latitude, longitude, dogParkArea, Picture, imageAlt FROM parks'); 
-		//$suburb1 = $pdo->query('SELECT distinct suburb  FROM parks');
-		}
-		catch (PDOException $e)  
-		{   
-		echo $e->getMessage();  
-		}
-		echo '<table>';
-			//echo'<select name="teacher">';
-				//echo'<option value="" selected="selected">SELECT STUDENT</option>';
-			echo'<tr>
-				<th>Course Name</th>
-				<th>Cost</th>
-				<th>Number of Lessons</th>
-				<th>Instrument</th>
-				<th>Available</th>
-				<th>Enroll</th>
-			</tr>';
-				foreach ($courses as $course){
-					echo'<tr>
-							<td>',$course['courseName'],'</td>
-							<td>',$course['courseCost'],'</td>
-							<td>',$course['numberOfLessons'],'</td>
-							<td>',$course['instrumentType'],'</td>
-							<td>',$course['available'],'</td>
-							<td><a href="enroll.php?course=',$course['courseID'],'">Enrol in Course</a></td>
-						</tr>';
-				}
-			//echo'</select>';
-			//echo'<input type="submit" name="SUBMIT">';
-		echo'</table>';
-
-	?>
 
 </div>
 
--- templates/lesson_template.php
+++ templates/lesson_template.php
@@ -15,44 +15,10 @@
 	include 'html_head.inc';
 ?>
 
-<div class="top-header">
+<div class="container-info">
 	
-	<div class="col-md-7">
-	</div>
-	
-	<div class="col-md-5">
-	</div>
-
-</div>
-
-<div class="home-page-content">
-	
-	<!--banner div should have fixed width-->
-	<div id="home-banner">
-	</div>
-
-	<div class="home-elements">
-		
-		<div class="col-md-3">
-		</div>
-
-		<div class="col-md-3">
-		</div>
-
-		<div class="col-md-3">
-		</div>
-
-		<div class="col-md-3">
-		</div>
-
-	</div>
-	<br><br>
-	<h1>Comming Soon</h1>
+	<h1>Coming Soon</h1>
 	<p>Soon you will be able to book a one on one tutor specialised tutoring.</p>
-	<?php
-		
-
-	?>
 
 </div>
 
